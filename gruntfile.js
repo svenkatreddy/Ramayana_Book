bac289c79a1ec96e346888c18b9b602118880249
unknown <u218711@DTC24008E599BE9.ent.wfb.bank.corp>
2014-04-16 16:38:03 -0700

    Adding languages folder

---
+++ gruntfile.js
@@ -0,0 +1,55 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-http-server');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                input: "./",
+                title: "Ramayana ",
+                description: "The Ramayana book.",
+                github: "svenkatreddy/Ramayana_Book"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '_book'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '_book'
+        },
+        'http-server': {
+            'dev': {
+                // the server root directory
+                root: '_book',
+
+                port: 4000,
+                host: "127.0.0.1",
+
+                showDir : true,
+                autoIndex: true,
+                defaultExt: "html",
+
+                //wait or not for the process to finish
+                runInBackground: false
+            }
+        }
+    });
+
+    grunt.registerTask('test', [
+        'gitbook',
+        'http-server'
+    ]);
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
